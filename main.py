f434438b1c9b45ab40c5d58dceb3a0c52c0c6795
Bernard Han <bernardhan@google.com>
2024-05-13 22:49:43 +0000

    test read arrow

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.feather as ft
+import datasets
+
+
+def main():
+    print(ft.read_table("/mnt/disks/ssd-array/dataset/train/data-00000-of-00078.arrow"))
+
+
+if __name__ == "__main__":
+    main()
