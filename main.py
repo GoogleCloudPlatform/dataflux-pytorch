e65373f8163a4c98113926e46808c4443a9c88b9
Bernard Han <bernardhan@google.com>
2024-05-14 17:26:37 +0000

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datasets import load_dataset
 
 
 def main():
-    df = pd.read_parquet("/mnt/disks/ssd-array/raw-dataset/train/test.parquet")
+    df = pd.read_parquet("/mnt/disks/ssd-array/raw-dataset/test.parquet")
     print(df.loc[0])
     print(df.dtypes)
     print(df.size)
