1f2a6d8a16defc5e9a10602c8106cb5317013d23
Bernard Han <bernardhan@google.com>
2024-05-14 18:08:33 +0000

    test

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import pyarrow as pa
-import pyarrow.feather as ft
+import pyarrow.parquet as pq
 import datasets
 from datasets import load_dataset
 
 
 def main():
-    df = pd.read_parquet("/mnt/disks/ssd-array/raw-dataset/test.parquet")
-    print(df.loc[0])
-    print(df.dtypes)
-    print(df.info())
+    files = [
+        "test.parquet",
+        "test1.parquet",
+        "test2.parquet",
+        "test3.parquet",
+        "test4.parquet",
+    ]
+
+    schema = pq.ParquetFile(files[0]).schema_arrow
+    with pq.ParquetWriter("output.parquet", schema=schema) as writer:
+        for file in files:
+            writer.write_table(pq.read_table(file, schema=schema))
 
 
 if __name__ == "__main__":
