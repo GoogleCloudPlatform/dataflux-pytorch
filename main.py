b639e21895cef866417d94feb0424da6f4850ffb
Bernard Han <bernardhan@google.com>
2024-05-14 00:38:04 +0000

    test read arrow

--- main.py
+++ main.py
@@ -2,22 +2,13 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.feather as ft
 import datasets
+from datasets import load_dataset
 
 
 def main():
-    ds = datasets.load_from_disk("/mnt/disks/ssd-array/dataset")
-    print(ds)
-
-    for train_instance in ds["train"]:
-        instruction = train_instance["instruction"]  # str
-        inputs = train_instance["inputs"]  # str
-        outputs = train_instance["outputs"]  # str
-        image_base64_str_list = train_instance["image_base64_str"]  # str (base64)
-        print(instruction)
-        print(inputs)
-        print(outputs)
-        print(image_base64_str_list)
-        break
+    ds_name = "imagenet-es"
+    dataset = load_dataset("MMInstruction/M3IT-80", ds_name)
+    dataset.save_to_disk("/mnt/disks/ssd-array/raw-dataset")
 
 
 if __name__ == "__main__":
