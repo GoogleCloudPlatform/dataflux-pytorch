9de93b2afdfd4ff9a204feceb22c2dc225835e3c
Bernard Han <bernardhan@google.com>
2024-05-29 17:43:22 +0000

    add create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -7,31 +7,6 @@ MODIFIED_RETRY = DEFAULT_RETRY.with_deadline(300.0).with_delay(
 )
 
 
-# def main():
-#     storage_client = storage.Client()
-#     source_bucket = storage_client.bucket("xai-hf-dataset-parquet-may-19")
-#     destination_bucket = storage_client.bucket("xai-hf-dataset-parquet-may-19")
-
-#     for i in range(529033):
-#         source_name = "000000.parquet"
-
-#         # This file represents the P10 of all files.
-#         if (i + 1) % 9 == 0:
-#             source_name = "000008.parquet"
-
-#         destination_name = "{:06d}".format(i) + ".parquet"
-
-#         source_blob = source_bucket.blob(source_name)
-#         source_bucket.copy_blob(
-#             source_blob, destination_bucket, destination_name, retry=MODIFIED_RETRY
-#         )
-#         print(f"Copied to destination object {destination_name}")
-
-
-# if __name__ == "__main__":
-#     main()
-
-
 def copy_blob_worker(workload, source_bucket_name, destination_bucket_name):
     """Worker function to copy blobs within its assigned workload."""
 
@@ -41,6 +16,8 @@ def copy_blob_worker(workload, source_bucket_name, destination_bucket_name):
 
     for i in workload:
         source_name = "000000.parquet"
+        # This is needed for P10 of the dataset to be a different data sample.
+        # Modify this to fit your need.
         if (i + 1) % 9 == 0:
             source_name = "000008.parquet"
         destination_name = f"{i:06d}.parquet"
@@ -52,17 +29,20 @@ def copy_blob_worker(workload, source_bucket_name, destination_bucket_name):
         print(f"Copied to destination object {destination_name}")
 
 
+# Utility function to use multi-processing to quickly create a dataset.
+# Usage: Modify the code directly for your use case and run it with
+#   python3 create_dataset.py.
 if __name__ == "__main__":
     import multiprocessing
 
-    # Replace with your actual bucket names
-    source_bucket_name = "xai-hf-dataset-parquet-may-19"
-    destination_bucket_name = "xai-hf-dataset-parquet-may-19"
+    # Replace with your actual bucket names.
+    source_bucket_name = "xai-hf-dataset-parquet-may-20"
+    destination_bucket_name = "xai-hf-dataset-parquet-may-20"
 
     num_processes = 128
-    total_files = 529033
+    total_files = 282257
 
-    # Split workload into chunks for each process
+    # Split workload into chunks for each process.
     chunk_size = total_files // num_processes
     workloads = []
     for i in range(num_processes):
@@ -70,7 +50,7 @@ if __name__ == "__main__":
         end = start + chunk_size if i < num_processes - 1 else total_files
         workloads.append(range(start, end))
 
-    # Create and start processes
+    # Create and start processes.
     processes = []
     for workload in workloads:
         process = multiprocessing.Process(
@@ -80,6 +60,6 @@ if __name__ == "__main__":
         processes.append(process)
         process.start()
 
-    # Wait for processes to finish
+    # Wait for processes to finish.
     for process in processes:
         process.join()
