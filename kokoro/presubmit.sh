276a04238ea87f4933cc30949931b00fba32f7ee
Bernard Han <103850980+bernardhan33@users.noreply.github.com>
2024-07-10 13:35:40 -0700

    Fix continuous and presubmit tests (#47)
    * attempt to fix continuous test by adding apt-get update
    
    * add '-y'
    
    * add more '-y'

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -21,8 +21,10 @@ set -e
 cd "${KOKORO_ARTIFACTS_DIR}/github/dataflux-pytorch"
 
 function setup_virtual_envs() {
+    sudo apt-get -y update
+
     echo Setting up Python virtual environment.
-    sudo apt install python3.8-venv
+    sudo apt install -y python3.8-venv
     python3 -m venv venv
     source venv/bin/activate
 }
