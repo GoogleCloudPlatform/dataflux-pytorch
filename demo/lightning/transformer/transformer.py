b78cc19a5ebf61f0afaa6767fa96ac3a7460b0b5
Matt Irvine <mirvine@google.com>
2024-07-13 19:39:49 +0000

    Save work on validation

--- demo/lightning/transformer/transformer.py
+++ demo/lightning/transformer/transformer.py
@@ -17,7 +17,6 @@
 import lightning
 import torch
 from lightning.pytorch import demos
-from torch.nn import functional
 from torch.utils import data
 
 dataset = demos.WikiText2()
@@ -54,12 +53,47 @@ class DatafluxTransformer(lightning.LightningModule):
     def configure_optimizers(self):
         return torch.optim.SGD(self.model.parameters(), lr=0.1)
 
+    def validation_step(self, batch, batch_idx):
+        inputs, target = batch
+        output = self.model(inputs, target)
+        output_idx = torch.multinomial(output.exp(), 1)
+        print(to_string(inputs[0]), to_string(output_idx), sep="\n\n")
+
+
+def to_string(input: torch.Tensor) -> str:
+    s = ""
+    for idx in input:
+        idx = idx.item()
+        s += dataset.dictionary.idx2word[idx] + " "
+    return s
+
+
+class ValidationDataset(data.Dataset):
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, index: int):
+        if index != 0:
+            raise Exception
+        words = [
+            dataset.dictionary.word2idx[word]
+            for word in "I am a language model , and I".split(" ")
+        ]
+        l = [torch.tensor(words).type(torch.int64)]
+        return torch.cat(l), torch.zeros(8).type(torch.int64)
+
 
 def main():
     dataloader = data.DataLoader(dataset)
     model = DatafluxTransformer(vocab_size=dataset.vocab_size)
-    trainer = lightning.Trainer(fast_dev_run=100)
+    trainer = lightning.Trainer(fast_dev_run=1000)
     trainer.fit(model=model, train_dataloaders=dataloader)
+    trainer.validate(model=model,
+                     dataloaders=data.DataLoader(ValidationDataset()))
 
 
 if __name__ == '__main__':
