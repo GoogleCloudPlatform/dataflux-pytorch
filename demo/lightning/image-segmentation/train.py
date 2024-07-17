00fbf338ee27c4cbd338b1b3498c88669914c0a7
Abhi Baireddy <abhibyreddi@google.com>
2024-07-17 16:38:36 -0700

    Add argument parser

--- demo/lightning/image-segmentation/train.py
+++ demo/lightning/image-segmentation/train.py
@@ -2,14 +2,11 @@ import lightning as pl
 
 from model import Unet3DLightning
 from data import Unet3DDataModule
+from arguments import PARSER
 
 if __name__ == "__main__":
-    # hard coded args for testing.
-    args = {
-        "a": 1,
-        "b": 2,
-    }
-    model = Unet3DLightning(args)
+    flags = PARSER.parse_args()
+    model = Unet3DLightning(flags)
     train_data_loader = Unet3DDataModule()
     trainer = pl.Trainer(devices=2, accelerator="gpu", max_epochs=5)
     trainer.fit(model=model, train_dataloaders=train_data_loader)
