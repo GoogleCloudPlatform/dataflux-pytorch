311230d1b1b08fb3d123549a52b60cec155e7e86
Abhi Baireddy <abhibyreddi@google.com>
2024-07-18 00:22:52 -0700

    Fix layers

--- demo/lightning/image-segmentation/layers.py
+++ demo/lightning/image-segmentation/layers.py
@@ -14,129 +14,171 @@
  limitations under the License.
  """
 
-import lightning.pytorch as pl
-import random
-import numpy as np
-from torch.utils.data import DataLoader
-from torch.utils.data import RandomSampler
-from torchvision import transforms
+import torch
+import torch.nn as nn
 
-from dataflux_pytorch import dataflux_mapstyle_dataset
-from pytorch_loader import DatafluxPytTrain
+activations = {
+    "relu": nn.ReLU(),
+    "leaky_relu": nn.LeakyReLU(0.01),
+    "sigmoid": nn.Sigmoid(),
+    "softmax": nn.Softmax(dim=1),
+    "none": nn.Identity(),
+}
+
+normalizations = {
+    "instancenorm": lambda n, _: nn.InstanceNorm3d(n, affine=True),
+    "batchnorm": lambda n, _: nn.BatchNorm3d(n),
+    "syncbatchnorm": lambda n, _: nn.SyncBatchNorm(n),
+    "none": lambda _, __: nn.Identity(),
+}
+
+convolutions = {"transpose": nn.ConvTranspose3d, "regular": nn.Conv3d}
 
 
-class Unet3DDataModule(pl.LightningDataModule):
+def _normalization(norm_type, num_features, num_groups=16):
+    if norm_type in normalizations:
+        return normalizations[norm_type](num_features, num_groups)
+    raise ValueError(f"Unknown normalization {norm_type}")
 
-    def __init__(self, args):
-        super().__init__()
-        self.args = args
 
-    def prepare_data(self):
-        pass
+def _activation(activation):
+    if activation in activations:
+        return activations[activation]
+    raise ValueError(f"Unknown activation {activation}")
 
-    def setup(self, stage="fit"):
-        if stage == "fit":
-            train_data_kwargs = {
-                "patch_size": self.args.input_shape,
-                "oversampling": self.args.oversampling,
-                "seed": self.args.seed,
-                "images_prefix": self.args.images_prefix,
-                "labels_prefix": self.args.labels_prefix,
-                "transforms": get_train_transforms(),
-            }
-            self.train_dataset = DatafluxPytTrain(
-                project_name=self.args.gcp_project,
-                bucket_name=self.args.gcs_bucket,
-                config=dataflux_mapstyle_dataset.Config(
-                    sort_listing_results=True),
-                **train_data_kwargs,
-            )
-            self.train_sampler = None
-            if self.args.num_workers > 1:
-                self.train_sampler = RandomSampler(self.train_dataset)
 
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.args.batch_size,
-            shuffle=not self.args.benchmark and self.train_sampler is None,
-            sampler=self.train_sampler,
-            num_workers=self.args.num_dataloader_threads,
-            persistent_workers=True,
-            pin_memory=True,
-            drop_last=True,
+def conv_block_factory(
+    in_channels,
+    out_channels,
+    kernel_size=3,
+    stride=1,
+    padding=1,
+    conv_type="regular",
+    name="",
+    norm_type="instancenorm",
+    activation="relu",
+):
+    suffix = "_conv" if conv_type == "regular" else "_deconv"
+    conv = convolutions[conv_type]
+    conv = conv(
+        in_channels,
+        out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        bias=norm_type == "none",
+    )
+
+    normalization = _normalization(norm_type, out_channels)
+    activation = _activation(activation)
+
+    return nn.Sequential(conv, normalization, activation)
+
+
+class DownsampleBlock(nn.Module):
+
+    def __init__(self, in_channels, out_channels, normalization, activation,
+                 index):
+        super(DownsampleBlock, self).__init__()
+        self.conv1 = conv_block_factory(
+            in_channels,
+            out_channels,
+            stride=2,
+            name=f"down{index}_block_0",
+            norm_type=normalization,
+            activation=activation,
+        )
+        self.conv2 = conv_block_factory(
+            out_channels,
+            out_channels,
+            name=f"down{index}_block_1",
+            norm_type=normalization,
+            activation=activation,
         )
 
-
-def get_train_transforms():
-    rand_flip = RandFlip()
-    cast = Cast(types=(np.float32, np.uint8))
-    rand_scale = RandomBrightnessAugmentation(factor=0.3, prob=0.1)
-    rand_noise = GaussianNoise(mean=0.0, std=0.1, prob=0.1)
-    train_transforms = transforms.Compose(
-        [rand_flip, cast, rand_scale, rand_noise])
-    return train_transforms
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
 
 
-class RandFlip:
+class UpsampleBlock(nn.Module):
 
-    def __init__(self):
-        self.axis = [1, 2, 3]
-        self.prob = 1 / len(self.axis)
+    def __init__(self, in_channels, out_channels, normalization, activation,
+                 index):
+        super(UpsampleBlock, self).__init__()
 
-    def flip(self, data, axis):
-        data["image"] = np.flip(data["image"], axis=axis).copy()
-        data["label"] = np.flip(data["label"], axis=axis).copy()
-        return data
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.upsample_conv = conv_block_factory(
+            in_channels,
+            out_channels,
+            kernel_size=2,
+            stride=2,
+            padding=0,
+            name=f"up{index}",
+            conv_type="transpose",
+            norm_type="none",
+            activation="none",
+        )
+        self.conv1 = conv_block_factory(
+            2 * out_channels,
+            out_channels,
+            name=f"up{index}_block_0",
+            norm_type=normalization,
+            activation=activation,
+        )
+        self.conv2 = conv_block_factory(
+            out_channels,
+            out_channels,
+            name=f"up{index}_block_1",
+            norm_type=normalization,
+            activation=activation,
+        )
 
-    def __call__(self, data):
-        for axis in self.axis:
-            if random.random() < self.prob:
-                data = self.flip(data, axis)
-        return data
-
-
-class Cast:
-
-    def __init__(self, types):
-        self.types = types
-
-    def __call__(self, data):
-        data["image"] = data["image"].astype(self.types[0])
-        data["label"] = data["label"].astype(self.types[1])
-        return data
-
-
-class RandomBrightnessAugmentation:
-
-    def __init__(self, factor, prob):
-        self.prob = prob
-        self.factor = factor
-
-    def __call__(self, data):
-        image = data["image"]
-        if random.random() < self.prob:
-            factor = np.random.uniform(low=1.0 - self.factor,
-                                       high=1.0 + self.factor,
-                                       size=1)
-            image = (image * (1 + factor)).astype(image.dtype)
-            data.update({"image": image})
-        return data
+    def forward(self, x, skip):
+        x = self.upsample_conv(x)
+        x = torch.cat((x, skip), dim=1)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
 
 
-class GaussianNoise:
+class InputBlock(nn.Module):
 
-    def __init__(self, mean, std, prob):
-        self.mean = mean
-        self.std = std
-        self.prob = prob
+    def __init__(self, in_channels, out_channels, normalization, activation):
+        super(InputBlock, self).__init__()
+        self.conv1 = conv_block_factory(
+            in_channels,
+            out_channels,
+            name="input_block_0",
+            norm_type=normalization,
+            activation=activation,
+        )
+        self.conv2 = conv_block_factory(
+            out_channels,
+            out_channels,
+            name="input_block_1",
+            norm_type=normalization,
+            activation=activation,
+        )
 
-    def __call__(self, data):
-        image = data["image"]
-        if random.random() < self.prob:
-            scale = np.random.uniform(low=0.0, high=self.std)
-            noise = np.random.normal(loc=self.mean,
-                                     scale=scale,
-                                     size=image.shape).astype(image.dtype)
-            data.update({"image": image + noise})
-        return data
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+
+
+class OutputLayer(nn.Module):
+
+    def __init__(self, in_channels, n_class):
+        super(OutputLayer, self).__init__()
+        self.conv = nn.Conv3d(in_channels,
+                              n_class,
+                              kernel_size=1,
+                              stride=1,
+                              padding=0,
+                              bias=True)
+
+    def forward(self, x):
+        return self.conv(x)
