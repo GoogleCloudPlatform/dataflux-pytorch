0d670281223f08950449ad61d464fd73c5800bdc
JD Nurme <jdnurme@google.com>
2024-07-19 19:39:27 +0000

    mvp

--- demo/lightning/text-based/model.py
+++ demo/lightning/text-based/model.py
@@ -1,8 +1,12 @@
 import torch
+import math
 from torch import optim, nn, utils, Tensor
 from torch.utils import data
-from dataflux_pytorch import dataflux_mapstyle_dataset
+from transformers import AutoTokenizer
+from dataflux_pytorch import dataflux_iterable_dataset as df_iter
 import lightning.pytorch as pl
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 # def parse_args():
 #     parser = argparse.ArgumentParser()
@@ -27,8 +31,13 @@ import lightning.pytorch as pl
 #     return parser.parse_args()
 
 # define any number of nn.Modules (or use your current ones)
-encoder = nn.Sequential(nn.Linear(10, 6), nn.ReLU(), nn.Linear(6, 3))
-decoder = nn.Sequential(nn.Linear(3, 6), nn.ReLU(), nn.Linear(6, 10))
+encoder = nn.Sequential(nn.Linear(25, 20), nn.ReLU(), nn.Linear(20, 10))
+decoder = nn.Sequential(nn.Linear(10, 20), nn.ReLU(), nn.Linear(20, 25))
+cols = [
+    'text', 'id', 'dump', 'url', 'date', 'file_path', 'language',
+    'language_score', 'token_count'
+]
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
 
 class TextDemoModel(pl.LightningModule):
@@ -45,25 +54,97 @@ class TextDemoModel(pl.LightningModule):
     def training_step(self, train_batch, batch_idx):
         # training_step defines the train loop.
         # it is independent of forward
-        x, _ = train_batch
+        #values_list = [float(len(val)) for val in train_batch.values()]
+        tokens = [tokenize(text) for text in train_batch["text"]]
+        x = torch.tensor(tokens)
+        #x, _ = train_batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
         x_hat = self.decoder(z)
         loss = nn.functional.mse_loss(x_hat, x)
         # Logging to TensorBoard (if installed) by default
-        self.log("train_loss", loss)
+        # self.log("train_loss", loss)
         return loss
 
 
-def format_data(content_in_bytes):
-    return content_in_bytes
+def tokenize(sequence):
+    tokens = tokenizer.tokenize(sequence, truncation=True)
+    ids = tokenizer.convert_tokens_to_ids(tokens)
+    ids = [float(x) for x in ids]
+    return ids[:25]
+
+
+def format_data(parquet_file):
+    """
+    The data comes in as a large parquet_file that must be read into a buffer and parsed
+    into the correct parquet format for processing.
+    """
+    reader = pa.BufferReader(parquet_file)
+    #table = pq.read_table(reader)
+    table = pq.ParquetFile(reader)
+    return table
+
+
+class ParquetIterableDataset(df_iter.DataFluxIterableDataset):
+
+    def __init__(self, columns, batch_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.columns = columns
+        self.batch_size = batch_size
+        self.current_file = None
+        self.columns = columns
+        """
+        An extension of the DataFluxIterableDataset that allows for iteration through subcomponents of
+        a downloaded Parquet file.
+        """
+
+    def __iter__(self):
+        worker_info = data.get_worker_info()
+        if worker_info is None:
+            print("Single-process data loading detected", flush=True)
+            for bytes_content in df_iter.dataflux_core.download.dataflux_download_lazy(
+                    project_name=self.project_name,
+                    bucket_name=self.bucket_name,
+                    objects=self.objects,
+                    storage_client=self.storage_client,
+                    dataflux_download_optimization_params=self.
+                    dataflux_download_optimization_params,
+                    retry_config=self.config.download_retry_config,
+            ):
+                table = self.data_format_fn(bytes_content)
+                for batch in table.iter_batches(batch_size=self.batch_size,
+                                                columns=self.columns):
+                    yield from batch.to_pylist()
+        else:
+            # Multi-process data loading. Split the workload among workers.
+            # Ref: https://pytorch.org/docs/stable/data.html#torch.utils.data.IterableDataset.
+            per_worker = int(
+                math.ceil(len(self.objects) / float(worker_info.num_workers)))
+            worker_id = worker_info.id
+            start = worker_id * per_worker
+            end = min(start + per_worker, len(self.objects))
+            for bytes_content in df_iter.dataflux_core.download.dataflux_download_lazy(
+                    project_name=self.project_name,
+                    bucket_name=self.bucket_name,
+                    objects=self.objects[start:end],
+                    storage_client=self.storage_client,
+                    dataflux_download_optimization_params=self.
+                    dataflux_download_optimization_params,
+                    retry_config=self.config.download_retry_config,
+            ):
+                table = self.data_format_fn(bytes_content)
+                for batch in table.iter_batches(batch_size=self.batch_size,
+                                                columns=self.columns):
+                    yield from batch.to_pylist()
 
 
 def main():
-    config = dataflux_mapstyle_dataset.Config(prefix="fineweb/sample/10BT/01")
+    config = df_iter.Config(prefix="fineweb/sample/10BT/014_00000.parquet")
     my_model = TextDemoModel(encoder, decoder)
 
-    dataset = dataflux_mapstyle_dataset.DataFluxMapStyleDataset(
+    dataset = ParquetIterableDataset(
+        columns=cols,
+        batch_size=9,
         project_name="gcs-tess",
         bucket_name="mirvine-benchmark-central1",
         config=config,
@@ -71,13 +152,12 @@ def main():
     )
     data_loader = data.DataLoader(
         dataset=dataset,
-        batch_size=1,
-        shuffle=True,
+        batch_size=9,
         num_workers=4,
         persistent_workers=True,
         pin_memory=True,
     )
-    trainer = pl.Trainer(limit_train_batches=2, max_epochs=1)
+    trainer = pl.Trainer(limit_train_batches=2, max_epochs=5)
     trainer.fit(model=my_model, train_dataloaders=data_loader)
 
 
