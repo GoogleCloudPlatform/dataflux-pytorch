2d34740aa6686ad1f342296150d268150003e36b
Yash9060 <Yash9060@users.noreply.github.com>
2024-08-22 18:53:58 +0000

    Adding custom GCS Writer

--- dataflux_pytorch/lightning/dataflux_lightning_checkpoint.py
+++ dataflux_pytorch/lightning/dataflux_lightning_checkpoint.py
@@ -2,16 +2,14 @@ from lightning.pytorch.plugins.io import CheckpointIO
 from google.cloud import storage
 from dataflux_core import user_agent
 from pathlib import Path
-from typing import Any, Dict, Optional, Union, Tuple
+from typing import Any, Dict, Optional, Union
 
 import torch
 import os
-import time
-import os
-import time
 from dataflux_core import user_agent
 from google.cloud import storage
 from lightning.pytorch.plugins.io import CheckpointIO
+from dataflux_pytorch.lightning.path_utils import parse_gcs_path
 
 
 class DatafluxLightningCheckpoint(CheckpointIO):
@@ -28,45 +26,13 @@ class DatafluxLightningCheckpoint(CheckpointIO):
             self.storage_client = storage.Client(project=self.project_name, )
         user_agent.add_dataflux_user_agent(self.storage_client)
 
-    def _process_input_path(self, path: Union[str, Path]) -> str:
-        if isinstance(path, str):
-            return path
-        elif isinstance(path, Path):
-            # When casting from Path object to string, it considers cloud URLs as Network URLs and gets rid of //
-            scheme, rest = str(path).split(":/")
-            return str(scheme) + "://" + str(rest)
-        else:
-            raise TypeError(
-                "path argument must be of type string or pathlib.Path object")
-
-    def _parse_gcs_path(self, path: Union[str, Path]) -> Tuple[str, str]:
-        if not path:
-            raise ValueError("Path cannot be empty")
-        input_path = self._process_input_path(path)
-        if not (input_path.startswith("gcs://")
-                or input_path.startswith("gs://")):
-            raise ValueError("Path needs to begin with gcs:// or gs://")
-        input_path = input_path.split("//", maxsplit=1)
-        if not input_path or len(input_path) < 2:
-            raise ValueError("Bucket name must be non-empty")
-        split = input_path[1].split("/", maxsplit=1)
-        bucket_name = ""
-        if len(split) == 1:
-            bucket_name = split[0]
-            prefix = ""
-        else:
-            bucket_name, prefix = split
-        if not bucket_name:
-            raise ValueError("Bucket name must be non-empty")
-        return bucket_name, prefix
-
     def save_checkpoint(
         self,
         checkpoint: Dict[str, Any],
         path: Union[str, Path],
         storage_options: Optional[Any] = None,
     ) -> None:
-        bucket_name, key = self._parse_gcs_path(path)
+        bucket_name, key = parse_gcs_path(path)
         rest_of_the_path, _ = os.path.split(key)
         os.makedirs(os.path.dirname(rest_of_the_path+"/"), exist_ok=True)
         with open(key, 'wb') as f:
@@ -86,7 +52,7 @@ class DatafluxLightningCheckpoint(CheckpointIO):
         path: Union[str, Path],
         map_location: Optional[Any] = None,
     ) -> Dict[str, Any]:
-        bucket_name, key = self._parse_gcs_path(path)
+        bucket_name, key = parse_gcs_path(path)
         bucket_client = self.storage_client.bucket(bucket_name)
         blob = bucket_client.blob(key)
         return torch.load(blob.open("rb"), map_location)
@@ -95,7 +61,7 @@ class DatafluxLightningCheckpoint(CheckpointIO):
         self,
         path: Union[str, Path],
     ) -> None:
-        bucket_name, key = self._parse_gcs_path(path)
+        bucket_name, key = parse_gcs_path(path)
         bucket_client = self.storage_client.bucket(bucket_name)
         blob = bucket_client.blob(key)
         blob.delete()
