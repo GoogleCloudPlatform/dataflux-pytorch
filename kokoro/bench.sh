3dfeff642dbe321f8825195602cc1e54e9c35761
abhibyreddi <160976060+abhibyreddi@users.noreply.github.com>
2024-09-06 13:33:01 -0700

    Run image training demo as part of continuous integ tests (#104)
    * Run image training demo as part of continuous integ tests
    
    * echo name of the benchmark that is being run
    
    * Pass --benchmark to train.py command

--- kokoro/bench.sh
+++ kokoro/bench.sh
@@ -50,13 +50,19 @@ function install_requirements() {
     echo Installing parquet demo requirements.
     pip install -r ./demo/lightning/text-based/distributed/requirements.txt
 
+    echo Installing image training demo requirements.
+    pip install -r ./demo/lightning/image-segmentation/requirements.txt
+
     echo Installing required dependencies.
     pip install .
 }
 
 function run_benchmarks(){
-    echo Running benchmarks.
+    echo Running benchmarks...
+    echo Running parquet-text benchmark.
     python3 -u ./demo/lightning/text-based/distributed/model.py --local --project=dataflux-project --bucket=fineweb-df-benchmark --num-workers=2 --num-nodes=1 --devices=5 --batch-size=512 --epochs=5 --limit-train-batches=1000 --log-level=ERROR;
+    echo Running image-segmentation benchmark.
+    python3 -u ./demo/lightning/image-segmentation/train.py --local --benchmark --gcp_project=dataflux-project --gcs_bucket=dataflux-demo-public --images_prefix=image-segmentation-dataset/images --labels_prefix=image-segmentation-dataset/labels --num_nodes=1 --num_devices=5 --epochs=2;
 }
 
 setup_virtual_envs
