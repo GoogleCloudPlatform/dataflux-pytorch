b7ec064a649494d1e8ea89a918863b6a23a87d12
Yash shah <Yash9060@users.noreply.github.com>
2024-09-09 19:01:00 -0700

    Adding GCS Custom reader (#105)
    * Adding GCS Custom reader
    
    * Adding GCS Custom reader

--- dataflux_pytorch/lightning/gcs_filesystem.py
+++ dataflux_pytorch/lightning/gcs_filesystem.py
@@ -6,7 +6,7 @@ from typing import Generator, Optional, Union, cast
 
 from dataflux_core import user_agent
 from google.cloud import storage
-from torch.distributed.checkpoint import FileSystemWriter
+from torch.distributed.checkpoint import FileSystemWriter, FileSystemReader
 from torch.distributed.checkpoint.filesystem import FileSystemBase
 from dataflux_pytorch.lightning.path_utils import parse_gcs_path
 
@@ -28,9 +28,18 @@ class GCSFileSystem(FileSystemBase):
     def create_stream(self, path: Union[str, os.PathLike],
                       mode: str) -> Generator[io.IOBase, None, None]:
         bucket, path = parse_gcs_path(path)
-        with self.storage_client.bucket(bucket).blob(path).open(
-                "wb", ignore_flush=True) as stream:
-            yield cast(io.IOBase, stream)
+        if mode == "wb":  # write mode.
+            with self.storage_client.bucket(bucket).blob(path).open(
+                    "wb", ignore_flush=True) as stream:
+                yield cast(io.IOBase, stream)
+        elif mode == "rb":  # read mode.
+            bucket_client = self.storage_client.bucket(bucket)
+            blob = bucket_client.blob(path)
+            blob_data = blob.download_as_bytes()
+            yield io.BytesIO(blob_data)
+        else:
+            raise ValueError(
+                "Invalid mode argument, create_stream only supports rb (read mode) & wb (write mode)")
 
     def concat_path(self, path: Union[str, os.PathLike],
                     suffix: str) -> Union[str, os.PathLike]:
@@ -88,3 +97,11 @@ class GCSDistributedWriter(FileSystemWriter):
         super().__init__(path, **kwargs)
         self.fs = GCSFileSystem(project_name, storage_client)
         self.sync_files = False
+
+
+class GCSDistributedReader(FileSystemReader):
+    def __init__(self, path: Union[str, os.PathLike],
+                 project_name: str,
+                 storage_client: Optional[storage.Client] = None):
+        super().__init__(path=path)
+        self.fs = GCSFileSystem(project_name, "read", storage_client)
