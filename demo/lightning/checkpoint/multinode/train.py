de5303dfc8496446107865537ccd75dae5c8a7b6
Yash9060 <Yash9060@users.noreply.github.com>
2024-09-26 18:31:25 +0000

    Simulated CPU multi node checkpointing

--- demo/lightning/checkpoint/multinode/train.py
+++ demo/lightning/checkpoint/multinode/train.py
@@ -3,7 +3,7 @@ import os
 import socket
 import time
 import torch
-
+import torch.multiprocessing as mp
 from contextlib import contextmanager
 from typing import Generator
 from lightning import Trainer
@@ -19,15 +19,14 @@ from dataflux_pytorch.lightning.path_utils import parse_gcs_path
 from dataflux_core import user_agent
 from dataflux_pytorch.lightning import DatafluxLightningCheckpoint
 from lightning.pytorch.trainer.states import TrainerFn
-from lightning.pytorch.strategies import FSDPStrategy
+from lightning.pytorch.strategies import DDPStrategy
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-
 from lightning.pytorch.strategies.fsdp import _METADATA_FILENAME
 from torch.distributed.checkpoint import save, load
 from torch.nn import Module
 
 
-class DatafluxFSDPStrategy(FSDPStrategy):
+class DatafluxFSDPStrategy(DDPStrategy):
 
     def __init__(self, path, project_name, storage_client, model, **kwargs):
         super().__init__(**kwargs)
@@ -121,122 +120,95 @@ class DatafluxFSDPStrategy(FSDPStrategy):
 
 def configure_master_addr():
     """Get coordinator IP Address with retries"""
-    coordinator_address = ""
-    coordinator_ip_address = ""
-    if os.environ.get("COORDINATOR_ADDRESS") is not None:
-        coordinator_address = os.environ.get("COORDINATOR_ADDRESS")
-        coordinator_found = False
-        lookup_attempt = 1
-        max_coordinator_lookups = 50
-        while not coordinator_found and lookup_attempt <= max_coordinator_lookups:
-            try:
-                coordinator_ip_address = socket.gethostbyname(
-                    coordinator_address)
-                coordinator_found = True
-            except socket.gaierror:
-                print(
-                    f"Failed to recognize coordinator address {coordinator_address} on"
-                    f" attempt {lookup_attempt}, retrying...")
-                lookup_attempt += 1
-                time.sleep(5)
-    print(f"Coordinator IP address: {coordinator_ip_address}")
-    os.environ["MASTER_ADDR"] = str(coordinator_ip_address)
+    coordinator_address = socket.gethostname()
+    coordinator_ip_address = socket.gethostbyname(coordinator_address)
+
+    os.environ["MASTER_ADDR"] = coordinator_ip_address
+    os.environ["MASTER_PORT"] = "12345"
 
 
-def init_processes():
+def init_processes(rank, world_size):
     """Initializes the distributed environment."""
-    # Get the necessary environment variables from the GKE environment
-    world_size = int(os.environ["WORLD_SIZE"])
-
-    job_index = int(os.environ.get("JOB_INDEX"))
-    job_completion_index = int(os.environ.get("JOB_COMPLETION_INDEX"))
-    processes_in_job = int(os.environ.get("PROCESSES_IN_JOB"))
-    rank = job_index * processes_in_job + job_completion_index
-    os.environ["NODE_RANK"] = str(rank)
-
+    os.environ['WORLD_SIZE'] = str(world_size)
+    os.environ['RANK'] = str(rank)
+    os.environ['NODE_RANK'] = str(rank)
     configure_master_addr()
 
 
-def main(project: str, ckpt_dir_path: str, save_only_latest: bool, ckpt_restore_path: str = ""):
-    if os.environ.get("COORDINATOR_ADDRESS"):
-        init_processes()
+def main(rank: int, world_size: int, project: str, ckpt_dir_path: str, save_only_latest: bool, ckpt_restore_path: str = ""):
+    init_processes(rank, world_size)
+
     dataset = WikiText2()
     dataloader = DataLoader(dataset, num_workers=1)
 
     model = DemoTransformer(vocab_size=dataset.vocab_size,
-                            nlayers=int(os.environ.get("NUM_LAYERS", 2)))
-    # Save once per step, and if `save_only_latest`, replace the last checkpoint each time.
-    # Replacing is implemented by saving the new checkpoint, and then deleting the previous one.
-    # If `save_only_latest` is False, a new checkpoint is created for each step.
+                            nlayers=int(os.environ.get("NUM_LAYERS", 10)))
+
     checkpoint_callback = ModelCheckpoint(
         save_top_k=1 if save_only_latest else -1,
         every_n_train_steps=1,
         filename="checkpoint-{epoch:02d}-{step:02d}",
         enable_version_counter=True,
     )
+
     accelerator = os.environ.get("ACCELERATOR", "cpu")
     min_epochs_save = os.environ.get("MIN_EPOCHS_SAVE", 4)
     max_epochs_save = os.environ.get("MAX_EPOCHS_SAVE", 5)
     max_steps_save = os.environ.get("MAX_STEPS_SAVE", 3)
-    trainer = Trainer(default_root_dir=ckpt_dir_path,
-                      plugins=[],
-                      callbacks=[checkpoint_callback],
-                      min_epochs=min_epochs_save,
-                      max_epochs=max_epochs_save,
-                      max_steps=max_steps_save,
-                      accelerator=accelerator,
-                      strategy=DatafluxFSDPStrategy(
-                          path=ckpt_dir_path,
-                          project_name=project,
-                          storage_client=None,
-                          model=model,
-                          state_dict_type="sharded",
-                      ),
-                      num_nodes=int(os.environ.get("WORLD_SIZE", 5))
-                      )
+
+    trainer = Trainer(
+        default_root_dir=ckpt_dir_path,
+        plugins=[],
+        callbacks=[checkpoint_callback],
+        min_epochs=min_epochs_save,
+        max_epochs=max_epochs_save,
+        max_steps=max_steps_save,
+        accelerator=accelerator,
+        strategy=DatafluxFSDPStrategy(
+            path=ckpt_dir_path,
+            project_name=project,
+            storage_client=None,
+            model=model,
+        ),
+        num_nodes=world_size
+    )
+
     trainer.fit(model, dataloader)
 
     print("Restoring checkpoints ...")
     min_epochs_restore = os.environ.get("MIN_EPOCHS_RESTORE", 4)
     max_epochs_restore = os.environ.get("MAX_EPOCHS_RESTORE", 5)
     max_steps_restore = os.environ.get("MAX_STEPS_RESTORE", 3)
+
     model = DemoTransformer(vocab_size=dataset.vocab_size,
                             nlayers=int(os.environ.get("NUM_LAYERS", 2)))
-    trainer = Trainer(default_root_dir=ckpt_dir_path,
-                      plugins=[],
-                      callbacks=[],
-                      min_epochs=min_epochs_restore,
-                      max_epochs=max_epochs_restore,
-                      max_steps=max_steps_restore,
-                      accelerator=accelerator,
-                      strategy=DatafluxFSDPStrategy(
-                          path=ckpt_restore_path,
-                          project_name=project,
-                          storage_client=None,
-                          model=model,
-                          state_dict_type="sharded",
-                      ),
-                      num_nodes=int(os.environ.get("WORLD_SIZE", 5))
-                      )
+    trainer = Trainer(
+        default_root_dir=ckpt_dir_path,
+        plugins=[],
+        callbacks=[],
+        min_epochs=min_epochs_restore,
+        max_epochs=max_epochs_restore,
+        max_steps=max_steps_restore,
+        accelerator=accelerator,
+        strategy=DatafluxFSDPStrategy(
+            path=ckpt_restore_path,
+            project_name=project,
+            storage_client=None,
+            model=model,
+            state_dict_type="sharded",
+        ),
+        num_nodes=world_size
+    )
     trainer.fit(model, dataloader, ckpt_path=ckpt_restore_path)
 
 
 class DemoTransformer(LightningTransformer):
-
-    def __init__(
-        self,
-        vocab_size: int = 33278,
-        nlayers: int = 2,
-    ) -> None:
+    def __init__(self, vocab_size: int = 33278, nlayers: int = 2) -> None:
         super().__init__()
         self.model = Transformer(vocab_size=vocab_size, nlayers=nlayers)
 
 
 if __name__ == "__main__":
-
-    main(
-        os.getenv("PROJECT"),
-        os.getenv("CKPT_DIR_PATH"),
-        os.getenv("SAVE_ONLY_LATEST") == "1",
-        os.getenv("CKPT_RESTORE_PATH"),
-    )
+    world_size = 2  # Number of simulated nodes
+    mp.spawn(main, args=(world_size, "gcs-tess", "gs://yashsha-us-east1-d/", os.getenv(
+        "SAVE_ONLY_LATEST") == "1", "gs://yashsha-us-east1-d/"), nprocs=world_size, join=True)
