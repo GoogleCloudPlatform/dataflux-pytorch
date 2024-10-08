cf280a1fee772920fd07eb026a6a447f6bce2159
Yash9060 <Yash9060@users.noreply.github.com>
2024-10-08 00:55:03 +0000

    Adding simulated cpu demo which can be used with cloudtop & integration test

--- demo/lightning/checkpoint/simulated/multiprocessing_train.py
+++ demo/lightning/checkpoint/simulated/multiprocessing_train.py
@@ -1,17 +1,34 @@
+"""
+ Copyright 2024 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ """
 import argparse
+import os
+import time
+import statistics
+from typing import Optional, Dict
+
 import torch
 import torch.nn as nn
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.distributed.checkpoint as dist_cp
-import os
-import time
-
 from lightning.pytorch.strategies import FSDPStrategy
-
 from dataflux_pytorch.lightning.gcs_filesystem import GCSDistributedWriter, GCSDistributedReader
-import statistics
-from typing import Optional, Dict
+
+MASTER_ADDR = 'localhost'
+MASTER_PORT = '12355'
 
 
 class BenchmarkStrategy(FSDPStrategy):
@@ -43,40 +60,52 @@ class SimpleModel(nn.Module):
         return self.fc2(torch.relu(self.fc1(x)))
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--project", type=str)
-    parser.add_argument("--ckpt-dir-path", type=str)
-    parser.add_argument("--model-size", type=int, default=100)
-    parser.add_argument("--clear-kernel-cache",
-                        action="store_true",
-                        default=False)
-    parser.add_argument("--sample-size", type=int, default=3)
-    parser.add_argument("--padding-size", type=int, default=1000)
-    parser.add_argument("--world-size", type=int)
+def parse_args() -> argparse.Namespace:
+    """Parses command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Benchmark distributed checkpointing.")
+    parser.add_argument("--project", type=str,
+                        required=True, help="GCS project ID.")
+    parser.add_argument("--ckpt-dir-path", type=str, required=True,
+                        help="Path to GCS bucket for checkpoints.")
+    parser.add_argument("--model-size", type=int,
+                        default=100, help="Size of the model.")
+    parser.add_argument("--clear-kernel-cache", action="store_true",
+                        default=False, help="Clear kernel cache.")
+    parser.add_argument("--sample-size", type=int, default=3,
+                        help="Number of samples for benchmarking.")
+    parser.add_argument("--padding-size", type=int, default=1000,
+                        help="Size of dummy tensors for padding, to control the size of the checkpoint.")
+    parser.add_argument("--world-size", type=int, required=True,
+                        help="Number of processes in the distributed setup.")
     return parser.parse_args()
 
 
 def format_size(size_bytes: int) -> str:
+    """Formats bytes into a human-readable size string."""
     size_mb = size_bytes / (1024 * 1024)
     return f"{size_mb / 1024:.2f} GB" if size_mb >= 1024 else f"{size_mb:.2f} MB"
 
 
 def get_tensor_size_bytes(tensor: torch.Tensor) -> int:
+    """Calculates the size of a tensor in bytes."""
     return tensor.element_size() * tensor.numel()
 
 
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+def setup(rank: int, world_size: int) -> None:
+    """Sets up the distributed environment."""
+    os.environ['MASTER_ADDR'] = MASTER_ADDR
+    os.environ['MASTER_PORT'] = MASTER_PORT
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
 
 def cleanup() -> None:
+    """Cleans up the distributed environment."""
     dist.destroy_process_group()
 
 
 def split_tensor(tensor: torch.Tensor, world_size: int, rank: int) -> torch.Tensor:
+    """Splits a tensor into chunks for distributed processing."""
     numel = tensor.numel()
     split_size = numel // world_size
     start_idx = rank * split_size
@@ -85,6 +114,7 @@ def split_tensor(tensor: torch.Tensor, world_size: int, rank: int) -> torch.Tens
 
 
 def time_checkpoint_operation(benchmark_strategy: BenchmarkStrategy, distributed_state_dict: Dict[str, torch.Tensor], filepath: str, sample_size: int, operation: str) -> list:
+    """Times save or load operations for checkpoints."""
     times = []
     for i in range(sample_size):
         start_time = time.time()
@@ -142,6 +172,15 @@ def run_benchmark(rank, world_size: int, model_size: int, project: str, filepath
 
 
 def main():
+    """
+    Typical usage example:
+
+      python3 -u demo/lightning/checkpoint/simulated/multiprocessing_train.py \
+      --project=<gcs_project_id> \
+      --ckpt-dir-path=<path_to_gcs_bucket> \
+      --model-size=300 \
+      --world-size=5
+    """
     args = parse_args()
 
     mp.set_start_method('spawn')
@@ -150,10 +189,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # world_size = int(os.getenv("WORLD_SIZE"))
-    # model_size = int(os.getenv("NUM_LAYERS"))
-    # project = os.getenv("PROJECT")
-    # path = os.getenv("CKPT_DIR_PATH")
-    # sample_size = int(os.getenv("SAMPLE_SIZE", 3))
-    # padding_size = int(os.getenv("PADDING_SIZE", 2000))
     main()
