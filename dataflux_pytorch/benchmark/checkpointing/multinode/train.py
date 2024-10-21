f0cd04f7bbed313d31091edd5b534de25e7fbb66
Abhi Baireddy <abhibyreddi@google.com>
2024-10-21 21:20:58 +0000

    Select default FSDP strategy when distributed_filesystem is set

--- dataflux_pytorch/benchmark/checkpointing/multinode/train.py
+++ dataflux_pytorch/benchmark/checkpointing/multinode/train.py
@@ -75,7 +75,8 @@ def get_strategy(args, project, model, ckpt_dir_path):
                                         project_name=project,
                                         state_dict_type="sharded",
                                         use_orig_params=False)
-    elif args.strategy == FSDP_STRATEGY and args.save_only:
+    elif (args.strategy == FSDP_STRATEGY
+          and args.save_only) or args.distributed_filesystem:
         print("Using FSDPStrategy.")
         strategy = FSDPStrategy(state_dict_type="sharded",
                                 use_orig_params=False)
