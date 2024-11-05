558d65298ffddb23d2869da745bb89a9c9264886
Adam Wonak <awonak@google.com>
2024-11-05 17:26:52 +0000

    remove irrelevant optimizer choice

--- dataflux_pytorch/benchmark/checkpointing/multinode/train_async_save.py
+++ dataflux_pytorch/benchmark/checkpointing/multinode/train_async_save.py
@@ -20,22 +20,14 @@ class DemoTransformer(LightningTransformer):
         self,
         vocab_size: int = 33278,
         nlayers: int = 2,
-        optimizer: str = "sgd",
         simulated_workload_duration: int = 0,
     ) -> None:
         super().__init__(vocab_size)
-        self.optimizer = optimizer
         self.simulated_workload_duration = simulated_workload_duration
         self.model = Transformer(vocab_size=vocab_size, nlayers=nlayers)
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
-        # Use self.trainer.model.parameters so that we can set
-        # use_orig_params=False on the Strategy. Using AdamW also results in a
-        # checkpoint size roughly 20% of used GPU memory.
-        if self.optimizer == "adamw":
-            return torch.optim.AdamW(self.trainer.model.parameters(), lr=0.1)
-        else:
-            return torch.optim.SGD(self.trainer.model.parameters(), lr=0.1)
+        return torch.optim.SGD(self.trainer.model.parameters(), lr=0.1)
 
     def forward(self, inputs, target):
         # Sleep for a few seconds to simulate cpu/gpu workload.
