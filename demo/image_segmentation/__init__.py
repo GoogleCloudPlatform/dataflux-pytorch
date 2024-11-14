aa31587a55d6d083e0fda43320ad5a60350ddd73
Akansha Maloo <amaloo@google.com>
2024-11-14 01:10:29 +0000

    change .model.unet3d path to absolute

--- demo/image_segmentation/__init__.py
+++ demo/image_segmentation/__init__.py
@@ -1,8 +1,8 @@
-from .model.unet3d import (Unet3D)
-from .model.losses import (DiceCELoss)
-from .model.layers import (DownsampleBlock, InputBlock, OutputLayer,
+from demo.image_segmentation.model.unet3d import (Unet3D)
+from demo.image_segmentation.model.losses import (DiceCELoss)
+from demo.image_segmentation.model.layers import (DownsampleBlock, InputBlock, OutputLayer,
                           UpsampleBlock)
-from .pytorch_loader import (get_train_transforms,RandBalancedCrop)
+from demo.image_segmentation.pytorch_loader import (get_train_transforms,RandBalancedCrop)
 
 __all__ = ["Unet3D","DiceCELoss", "DownsampleBlock", "InputBlock", "OutputLayer",
                           "UpsampleBlock","get_train_transforms", "RandBalancedCrop"]
